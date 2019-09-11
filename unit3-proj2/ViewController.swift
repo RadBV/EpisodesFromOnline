8000b736c3d6ec429c2162b324a4e936cb3b4ec7
Radharani Ribas-Valongo <rad@Radharanis-MacBook-Pro.local>
2019-09-11 16:34:04 -0400

    initial commit

---
+++ unit3-proj2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  unit3-proj2
+//
+//  Created by Radharani Ribas-Valongo on 9/11/19.
+//  Copyright © 2019 Radharani Ribas-Valongo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ unit3-proj2Tests/unit3_proj2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  unit3_proj2Tests.swift
+//  unit3-proj2Tests
+//
+//  Created by Radharani Ribas-Valongo on 9/11/19.
+//  Copyright © 2019 Radharani Ribas-Valongo. All rights reserved.
+//
+
+import XCTest
+@testable import unit3_proj2
+
+class unit3_proj2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
