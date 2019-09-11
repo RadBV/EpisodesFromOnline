0790863e1931c5254568aeb0b8f975d62dcc087e
Radharani Ribas-Valongo <rad@Radharanis-MacBook-Pro.local>
2019-09-11 19:59:43 -0400

    creates NetworkManager & ErrorHandling files, organizes everything into seperate folders

---
+++ unit3-proj2/Models/ShowsAPIClient.swift
@@ -0,0 +1,9 @@
+//
+//  ShowsAPIClient.swift
+//  unit3-proj2
+//
+//  Created by Radharani Ribas-Valongo on 9/11/19.
+//  Copyright © 2019 Radharani Ribas-Valongo. All rights reserved.
+//
+
+import Foundation
